dca169e3c0b6194c7efb1440aaab9133b416bd83
Temur <temur.fatkulin@gmail.com>
2017-11-26 10:09:27 +0300

    fix

--- models/game.rb
+++ models/game.rb
@@ -29,7 +29,7 @@ class Game < ActiveRecord::Base
   end
 
   def make_move(player, index)
-    if next_player.user == player
+    if next_player.user.id == player.id
       board.place_symbol(index, next_player.game_symbol)
       self.data['board'] = board.cells
       self.data_will_change!
